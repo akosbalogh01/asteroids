f0c8bdabb8bdeb51305acdb1819150a35a5c1693
Ákos <akos.balogh01@gmail.com>
2018-11-13 16:38:58 +0100

    render score

--- asteroids/src/game.h
+++ asteroids/src/game.h
@@ -4,27 +4,33 @@
 #define WIDTH 8
 #define HEIGHT 3
 
+#include "segmentlcd_individual.h"
+typedef SegmentLCD_UpperCharSegments_TypeDef upperseg;
+typedef SegmentLCD_LowerCharSegments_TypeDef lowerseg;
+
 typedef enum {NEW_GAME, RUNNING, LEVEL_UP, GAME_OVER} game_status;
 
 typedef struct {
-	unsigned char right : 1;
-	unsigned char left : 1;
-	unsigned char start : 1;
-	unsigned char ragequit: 1;
+	uint8_t right : 1;
+	uint8_t left : 1;
+	uint8_t start : 1;
+	uint8_t ragequit: 1;
 } IRQ_VARIABLES;
 
 typedef struct {
 	game_status status;
-	unsigned char level;
-	unsigned char asteroids[3]; //3 indexes of where the asteroids are
-	unsigned char spaceship;	//index of spaceship
-	unsigned char direction : 2; //
+	uint8_t  level;
+	uint8_t  asteroids[3]; //asteroid indexes
+	uint8_t  spaceship;	//index of spaceship
+	uint8_t	 direction : 2; //
+	upperseg numerical[SEGMENT_LCD_NUM_OF_UPPER_CHARS];
+	lowerseg alphanumerical[SEGMENT_LCD_NUM_OF_LOWER_CHARS];
 } logic;
 
 // D: interrupt function prototypes
 // interrupts.c -> #include "game.h"
 
-// A: render function prototypes
+void render_score(uint8_t);
 
 
 #endif //ASTEROIDS_GAME
---
+++ asteroids/src/render_score.c
@@ -0,0 +1,20 @@
+#include "segmentlcd_individual.h"
+#include "game.h"
+#include "charmap.h"
+
+upperseg numbers[] = {{CHARMAP_0}, {CHARMAP_1}, {CHARMAP_2}, {CHARMAP_3}, {CHARMAP_4}, {CHARMAP_5}, {CHARMAP_6}, {CHARMAP_7}, {CHARMAP_8}, {CHARMAP_9}};
+upperseg result[SEGMENT_LCD_NUM_OF_UPPER_CHARS];
+
+void render_score(uint8_t level) {
+	uint8_t i = 0;
+	register uint8_t temp;
+
+	do {
+		temp = level % 10;
+		result[i] = numbers[temp];
+		level -= temp;
+		level /= 10;
+	} while(i < 3);
+
+	SegmentLCD_UpperSegments(result);
+};
