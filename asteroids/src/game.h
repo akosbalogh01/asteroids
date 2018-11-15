21989d83de1d096f926f0a057e6c9b34ed30f2b3
akosbalogh01 <akos.balogh01@gmail.com>
2018-11-15 08:29:32 +0100

    comments

--- asteroids/src/game.h
+++ asteroids/src/game.h
@@ -11,41 +11,41 @@
 #endif
 
 #include "segmentlcd_individual.h"
-typedef SegmentLCD_UpperCharSegments_TypeDef upperseg;
+typedef SegmentLCD_UpperCharSegments_TypeDef upperseg;	//redefinition of Segment data structures, to avoid cluttered code
 typedef SegmentLCD_LowerCharSegments_TypeDef lowerseg;
 
-typedef enum {NEW_GAME, RUNNING, LEVEL_UP, GAME_OVER} game_status;
-typedef enum {HORIZONTAL, UP, DOWN} s_ori;
+typedef enum {NEW_GAME, RUNNING, LEVEL_UP, GAME_OVER} game_status;	//state machine available states
+typedef enum {HORIZONTAL, UP, DOWN} s_ori;	//spaceship available orientations
 
-typedef struct {
-	uint8_t right 	: 1;
-	uint8_t left 	: 1;
-	uint8_t start 	: 1;
-	uint8_t ragequit: 1;
+typedef struct {						//variables modified by global interrupts, enabled for one cycle
+	uint8_t right 	: 1;				//enabled when <right> is pressed, serial port R
+	uint8_t left 	: 1;				//enabled when <left> is pressed, serial port L
+	uint8_t start 	: 1;				//enabled when <start> is pressed, serial port S
+	uint8_t ragequit: 1;				//enabled when <ragequit> is pressed, serial port R
 } IRQ_VARIABLES;
 
-typedef struct {
+typedef struct {						//game descriptor data structure
 	game_status status;
 	IRQ_VARIABLES params;
-	int	     level;
-	uint8_t  asteroids[3]; 	//asteroid indexes
-	uint8_t  spaceship;		//spaceship index
-	s_ori	 orientation; 	//spaceship orientation
-	uint16_t movecycle;
+	int	     level;						//current level
+	uint8_t  asteroids[3]; 				//asteroid indexes
+	uint8_t  spaceship;					//spaceship index
+	s_ori	 orientation; 				//spaceship orientation
+	uint16_t movecycle;					//current movecycle (how many cycles between spaceship moving)
 } logic;
 
-void initialize();
+void initialize();						//initializes hardware components (CHIP, TIMER, UART, LCD)
 
-void spaceship_rotate(logic*);
-int  spaceship_move(logic*);
+void spaceship_rotate(logic*);			//rotates spaceship during the next cycle, based on IRQ modified global variables
+int  spaceship_move(logic*);			//moves spaceship during the next <move> cycle, -
 
-int  decrement(int, int);
-void generate_field(logic*);
+int  decrement(int, int);				//decrements move cycle, based on level, and previous move cycle
+void generate_field(logic*);			//generates asteroid field #todo: do not allow repeated selection of the same segment
 
-void render_score(uint8_t);
-void render_text_clear();
-void render_text_newgame();
-void render_text_gameover();
-void render_level(uint8_t, uint8_t*);
+void render_score(uint8_t);				//renders level on <upper> segment LCD
+void render_text_clear();				//deprecated
+void render_text_newgame();				//deprecated
+void render_text_gameover();			//deprecated
+void render_level(uint8_t, uint8_t*);	//renders spaceship and asteroids
 
 #endif //ASTEROIDS_GAME
--- asteroids/src/main.c
+++ asteroids/src/main.c
@@ -7,12 +7,15 @@
 #include "interrupt.h"
 
 int main(void) {
+	//initialize hardware and software components
 	initialize();
 
 	logic program = {0};
 	program.status = NEW_GAME;
 
 	while (1) {
+		//atomic copy of <IRQ modifiable> global variables, to game descriptor
+		//business logic is done entirely based on the game game descriptor structure
 		CORE_CRITICAL_SECTION(
 			program.params = irq_var;
 			irq_var.start = 0;
@@ -22,9 +25,10 @@ int main(void) {
 		)
 
 		switch (program.status) {
+		//new game, wait for <Start>
 		case NEW_GAME:
 			SegmentLCD_Write("NEWGAME");
-			if (program.params.start) {
+			if (program.params.start) { //if start was pressed, move to <Running>
 				SegmentLCD_AllOff();
 				program.status    	 = RUNNING;
 				program.level 	  	 = 0;
@@ -39,6 +43,7 @@ int main(void) {
 			else break;
 			/* no break */
 
+		//level up, initializes and modifies required variables, be it starting a new game, or moving between levels
 		case LEVEL_UP:
 			program.level++;
 			program.movecycle = decrement(program.level, program.movecycle);
@@ -48,6 +53,7 @@ int main(void) {
 			program.status = RUNNING;
 			/* no break */
 
+		//game is running
 		case RUNNING:;
 			uint8_t i;
 			for (i = 0; i < 3; i++) {if (program.asteroids[i] == program.spaceship) break;}
@@ -57,7 +63,7 @@ int main(void) {
 			spaceship_rotate(&program);
 			render_level(program.spaceship, program.asteroids);
 
-			if (flag) {
+			if (flag) {	//flag is enabled every time the TIMER0 overflows, maximum of T0 equals to <movecycle>
 				switch (spaceship_move(&program)) {
 				case  1: program.status = LEVEL_UP;  break;
 				case -1: program.status = GAME_OVER; break;
@@ -73,7 +79,8 @@ int main(void) {
 			}
 
 			break;
-
+		
+		//game over
 		case GAME_OVER:
 			SegmentLCD_Number(program.level);
 			SegmentLCD_Write("RIP");
